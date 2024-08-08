b685473aeb53e787df327c45a60057877565e587
joshua mwendwa <leeroyjoshu087@gmail.com>
2024-08-09 00:36:34 +0300

    JLEE

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
+
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 import itemData from "../data/items";
+import { v4 as uuid } from "uuid";
 
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  function onItemFormSubmit(newItem) {
+    setItems([...items,newItem])
+  }
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
@@ -14,7 +19,10 @@ function App() {
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList
+        items={items}
+        onItemFormSubmit={onItemFormSubmit}
+      />
     </div>
   );
 }
